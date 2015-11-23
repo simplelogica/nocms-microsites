adcbfcfc887be9535e6781dd56ce164473627d37
zapic0 <zapico@gmail.com>
2015-11-23 17:06:53 +0100

    Initializes micrositer middleware to be used by gem

--- lib/no_cms/microsites/engine.rb
+++ lib/no_cms/microsites/engine.rb
@@ -2,6 +2,10 @@ module NoCms
   module Microsites
     class Engine < ::Rails::Engine
       isolate_namespace NoCms::Microsites
+
+      initializer "microsites.middleware" do |app|
+        app.config.app_middleware.use "NoCms::Microsites::Micrositer"
+      end
     end
   end
 end
