c40858773ac7c01ecf952336cd524d4b4db43fd4
zapic0 <zapico@gmail.com>
2015-11-23 10:12:21 +0100

    Helper to write urls looking after microsite

--- app/helpers/nocms_microsites/application_helper.rb
+++ app/helpers/nocms_microsites/application_helper.rb
@@ -1,4 +1,15 @@
 module NoCms::Microsites
   module ApplicationHelper
+    # Method used to clean urls if we are drawing a url from a microsite host
+    # so we can draw urls removing "microsite root"
+    def clean_for_microsites url
+      host = request.host
+      microsite = NoCms::Microsites::Microsite.find_by_domain(host)
+      unless microsite.blank?
+        url = url.gsub(microsite.root_path, "")
+      end
+      url
+    end
+
   end
 end
